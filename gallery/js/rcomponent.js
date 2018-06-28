d7e3d3afcdde24675d18ba4b5faa05cb788fc52a
rowm <ranwei952005@163.com>
2018-06-28 19:58:16 +0800

    modify nav,content

--- gallery/js/rcomponent.js
+++ gallery/js/rcomponent.js
@@ -1,3 +1,12 @@
 
 
 
+
+// set nav height start
+
+function fsetNavHeight () {
+	var e = document.getElementsByClassName('nav')[0];
+	e.style.height = document.documentElement.clientHeight - 65 + 'px';
+}
+
+// set nav heigth end
